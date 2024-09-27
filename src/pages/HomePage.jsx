e1774cb97bab276cbbf2c4630d7ec21fc96834da
Varunkmr481 <kmrvarun481@gmail.com>
2024-09-27 15:58:47 +0530

    Link between routes with <NavLink />

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,10 @@
+import PageNav from "../components/PageNav";
+
 export default function HomePage() {
-  return <div>WorldwISE</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>WORLDWISE</h1>
+    </div>
+  );
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,12 @@
+import PageNav from "../components/PageNav";
+
 function Product() {
-  return <div>product</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Product</h1>
+    </div>
+  );
 }
 
 export default Product;
