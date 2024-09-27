db0ee313e37083def42cace63bc8ef1edf53a43d
Varunkmr481 <kmrvarun481@gmail.com>
2024-09-27 19:59:33 +0530

    using css modules

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <h1>Welcome to the app</h1>
+    </div>
+  );
+}
+
+export default AppLayout;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 export default function HomePage() {
   return (
     <div>
       <PageNav />
-      <h1>WORLDWISE</h1>
+      <AppNav />
+      <h1 className="test">WORLDWISE</h1>
+
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
