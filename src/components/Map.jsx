be8b059603a79bcfbadb11cd2dc69c25db121a33
Varunkmr481 <kmrvarun481@gmail.com>
2024-10-04 15:52:24 +0530

    Interacting with map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,21 +1,42 @@
 import styles from "./Map.module.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import "flag-icons/css/flag-icons.min.css";
 // eslint-disable-next-line
 import getCountryCodeFromEmoji from "../getCountryCodeFromEmoji";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function Map() {
   const { cities } = useCities();
   // eslint-disable-next-line no-unused-vars
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
+  // extract lat&lng from url & store in mapPosition
+  const [searchParams] = useSearchParams();
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
+
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        // center={[mapLat, mapLng]}
+        zoom={7}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -36,9 +57,29 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+
+/* eslint-disable-next-line */
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+      // console.log(e);
+    },
+  });
 }
 
 export default Map;
